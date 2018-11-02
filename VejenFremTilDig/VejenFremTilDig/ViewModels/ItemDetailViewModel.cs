190355b6fb56c29b64d96830d589b550228c9421
Minik Gaarde Lambrecht <mini0040@edu.sde.dk>
2018-11-02 08:24:52 +0100

    Add project files.

---
+++ VejenFremTilDig/VejenFremTilDig/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using VejenFremTilDig.Models;
+
+namespace VejenFremTilDig.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
