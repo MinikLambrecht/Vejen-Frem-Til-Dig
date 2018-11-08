52df8da2e0d67f3c6ace5f436da963b543bf0fea
Minik Gaarde Lambrecht <mini0040@edu.sde.dk>
2018-11-08 15:23:49 +0100

    Updates - Call page layout finished - Message & Video page layout *WIP*

---
+++ VejenFremTilDig/VejenFremTilDig/Services/ContactListViewModel.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using VejenFremTilDig.Models;
+
+namespace VejenFremTilDig.Services
+{
+    public class ContactListViewModel : BaseViewModel
+    {
+        public ObservableCollection<Contact> Contacts { get; set; }
+
+        public ContactListViewModel()
+        {
+            Contacts = new ObservableCollection<Contact>
+            {
+                new Contact
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Name = "Lone",
+                    Number = 97485279
+                },
+                new Contact
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Name = "Bodil",
+                    Number = 64558739
+                },
+                new Contact
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Name = "IB",
+                    Number = 81225798
+                },
+                new Contact
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Name = "Åse",
+                    Number = 82792684
+                },
+                new Contact
+                {
+                    Id = Guid.NewGuid().ToString(),
+                    Name = "Rasmus",
+                    Number = 10557916
+                }
+            };
+        }
+
+        public void AddContact(ObservableCollection<Contact> contacts)
+        {
+            Contact contact = new Contact { Id = Guid.NewGuid().ToString(), Name = "Bob", Number = 76889452 };
+            contacts.Add(contact);
+        }
+    }
+}
--- VejenFremTilDig/VejenFremTilDig/Views/ContactsPage.xaml.cs
+++ VejenFremTilDig/VejenFremTilDig/Views/ContactsPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
-using System.Collections.ObjectModel;
+using System.Collections.Generic;
 using VejenFremTilDig.Models;
+using VejenFremTilDig.Services;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -9,9 +10,19 @@ namespace VejenFremTilDig.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ContactsPage : ContentPage
     {
+        ContactListViewModel ListviewModel;
+
         public ContactsPage()
         {
             InitializeComponent();
+
+            ListviewModel = new ContactListViewModel();
+            BindingContext = ListviewModel;
+        }
+
+        public void OnAddClick(object sender, EventArgs e)
+        {
+            DisplayAlert("Tapped!", "You have tapped the image!", "OK");
         }
     }
 }
